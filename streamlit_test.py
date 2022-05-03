e34bf27dcc67a383580965ee750ea2aac4aad8f5
mallika-vgiri <mallika.vgiri@gmail.com>
2022-05-03 16:08:23 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -19,8 +19,11 @@ import pandas as pd
 import numpy as np
 import time
 
-st.title('Uber pickups in NYC')
-st.write("Hello")
+st.title('Employee Attrition Predictor')
+st.write("Predict whether an employee is at risk of leaving the organization using this Employee Attrition Predictor tool. This tool, developed using advanced machine learning techniques, predicts employee attrition with over 98% accuracy")
+
+st.image("https://blog.mavenlink.com/hubfs/employee-turnover-blog-image.png")
+
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
 
