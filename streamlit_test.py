3d28e6c845ec84b9dc7169dc8077e455625a0a15
mallika-vgiri <mallika.vgiri@gmail.com>
2022-05-03 18:21:22 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -16,12 +16,12 @@ with st.form(key='my_form'):
     number_project = st.number_input('Number of projects the employee has worked on',0)
     avg_monthly_hours = st.number_input('On an average, how many hours did the employee work each month?',0)
     time_spend_company = st.number_input('How many years has the employee spent in the company?',0)
-    work_accident = st.text_input('Has the employee encountered a work accident during their tenure?',0)
-    promotion_last_5years = st.text_input('Has the employee been promoted in the last 5 years?',0)
+    work_accident = st.text_input('Has the employee encountered a work accident during their tenure?')
+    promotion_last_5years = st.text_input('Has the employee been promoted in the last 5 years?')
     submit_button = st.form_submit_button(label='Submit')
 
 def txt_num(val):
-    if val ='Yes'
+    if val == "Yes"
         return 1
     else
         return 0
