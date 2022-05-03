94accc85626ac83421a822734b1f0b494735d69c
mallika-vgiri <mallika.vgiri@gmail.com>
2022-05-03 14:30:35 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -17,8 +17,5 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
 
-st.set_page_config(
-    page_title="A/B Testing App", page_icon="📊", initial_sidebar_state="expanded"
-)
+st.write("Hello")
