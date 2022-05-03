89d7553c278df26f3342bfa7216026318b474ed2
mallika-vgiri <mallika.vgiri@gmail.com>
2022-05-03 16:56:32 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -20,31 +20,3 @@ d = {'satisfaction_level':satisfaction_level,'last_evaluation':last_evaluation,'
 df=pd.DataFrame(data=d,index=[0])
 
 st.write(df.head())
-"""
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-    
-
-    st.write("This code will be printed to the sidebar.")
-
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
-    
-container = st.container()
-container.write("This is inside the container")
-st.write("This is outside the container")
-
-# Now insert some more in the container
-container.write("This is inside too")
-"""
